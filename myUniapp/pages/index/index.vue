28ef7d54f893ca7e0d3f248fd05dd121b9733ffe
suzhengtang <1037617576@qq.com>
2021-05-08 10:31:34 +0800

    完成首页商品展示

--- myUniapp/pages/index/index.vue
+++ myUniapp/pages/index/index.vue
@@ -10,11 +10,22 @@
 		<view class="nav">
 			<view class="nav-item" v-for="item in navData" :key="item.id" @click="itemClick(item.id)">
 				<view class="icon" :class="item.color"></view>
-				<text class="icon-text">{{item.text}}</text>
+				<text class="icon-text">{{ item.text }}</text>
 			</view>
 		</view>
 		<view class="tuijian">
-			推荐商品
+			{{ title }}
+		</view>
+		<view class="main-item">
+			<view class="item-one" v-for="(item, index) in itemArr" :key="index">
+				<view class="one-img">
+					<image class="one-img-img" mode="widthFix" src="../../static/images/meinv.png"></image>
+				</view>
+				<view class="one-dir">
+					<view class="dir-top"><span>美女</span><span class="remove-el">帅哥</span></view>
+					<view class="dir-bottom">我很漂亮并且也很友好，和我交朋友的人都很幸运</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -33,11 +44,13 @@
 				autoplay: true,
 				interval: 2000,
 				navData: [
-					{text: '正堂练习', id: 11, color: "red"},
+					{text: '商品列表', id: 11, color: "red"},
 					{text: '联系我们', id: 12, color: "yellow"},
 					{text: '社区图片', id: 13, color: "pink"},
 					{text: '学习视频', id: 14, color: "skyblue"},
-				]
+				],
+				title: '推荐商品',
+				itemArr: [1,2,3,4,5,6,7,8,9,0]
 			}
 		},
 		onLoad() {
@@ -88,7 +101,7 @@
 	}
 	.content .nav .icon-text {
 		font-size: 12px;
-		color: #333;
+		color: #666;
 		display: inline-block;
 	}
 	.content .tuijian{
@@ -100,5 +113,49 @@
 		color: #ed7043;
 		letter-spacing: 60rpx;
 		text-align: center;
+	}
+	.content .main-item {
+		padding: 0 0 20rpx 20rpx;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.content .main-item .item-one {
+		width: 345rpx;
+		background-color: #fff;
+		padding: 30rpx;
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		box-sizing: border-box;
+		margin: 0 20rpx 20rpx 0;
+	}
+	.content .main-item .item-one .one-img {
+		width: 100%;
+		text-align: center;
+	}
+	.content .main-item .item-one .one-img .one-img-img{
+		width: 100%;
+	}
+	.content .main-item .item-one .one-dir{
+		color: #666;
+		font-size: 30rpx;
+	}
+	.content .main-item .item-one .one-dir .dir-top{
+		color: #ed7043;
+		font-size: 34rpx;
+	}
+	.content .main-item .item-one .one-dir .dir-top .remove-el{
+		color: #ccc;
+		font-size: 30rpx;
+		margin-left: 30rpx;
+		text-decoration: line-through;
+		
+	}
+	.content .main-item .item-one .one-dir .dir-bottom{
+		text-overflow: ellipsis;
+		overflow: hidden;
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 2;
+		margin-top: 20rpx;
 	}
 </style>
